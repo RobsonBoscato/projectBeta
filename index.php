829f557d70eef639514e6375078f1734ff57f9ff
RobsonBoscato <rbsn.boscato@gmail.com>
2022-07-25 12:40:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <title>Project Beta</title>
+  
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<header>
+  <h1>Project BETA Manager</h1>
+  <p>Bem vindo ao sistema de gerenciamento e gestão de partidas</p>
+
+  <p><strong>Afinal, não é porquê o jogo é amistoso, que não exige compromisso.</strong>
+  </p>
+</header>
+
+<body>
+
+  <nav>
+
+    <ul>
+      <li>Home <a href="index.php"></a></li>
+      <li>Sobre</li>
+      <li>Login</li>     
+    </ul>
+
+  </nav>
+
+  <div class="center">
+    <p>O propósito da criação desta facilidade, se resume a otimizar o tempo de gestão de atividades esportivas, das mais diversas áreas, podendo ser adaptado até para marcações de eventos...
+    <br>
+
+      <nav>
+        <ol>
+          <span>E tudo isso de maneira simples e prática:</span>
+        
+          <li>Você cria o evento;</li>
+          <li>Registra todas a informações pertinentes;</li>
+          <li>Disponibiliza o link com todas as informações</li>
+
+        </ol>
+      </nav>
+
+    E todos os que possuem cadastro em nosso site, podem confirmar a presença.</p>
+  </div>
+
+  <footer>
+    <p>Conheça mais sobre nossa empresa em nossas redes sociais:</p>
+    <ul>
+          
+      <li><a href="https:instagram.com/betamanager" title="Instagram" target="_blank"><img id="instagram" src="images/insta.png" alt="logo instagram"></a></li>
+
+      <li><a href="https:facebook.com/betamanager" title="Facebook" target="_blank" ><img id="facebook" src="images/facebook.png" alt="logo facebook"></a></li>
+
+      <li><a href="https:twitter.com/betamanager" title="Twitter" target="_blank"  ><img id="twitter" src="images/twitter.png" alt="logo twitter"></a></li>
+    </ul>
+    
+  </footer>  
+  
+</body>
+</html>
